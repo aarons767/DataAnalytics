8de16ef51034bd97df8bfb797ab5b7062fc38514
Aaron <aarondsouza42@gmail.com>
2024-11-26 14:18:39 -0800

    Main csv file is cleaned and noramalized. Run shell script first then python script. Inconsistent addresses are normalized to N/A and empty Killed or injured fields are deleted from the data set

--- cleaner_script.sh
+++ cleaner_script.sh
@@ -14,13 +14,25 @@ awk -F, 'NR == 1 { print; next } !seen[$0]++' $og_file > $cleaned_file
 
 
 echo "Removing rows with missing column data"
-awk -F, '{
+awk -F ',' '{
     # Check if any of the fields are empty
-    if ($1 == "" || $2 == "" || $3 == "" || $4 == "" || $5 == "" || $6 == "" || $7 == "") {
-        # Use system to print the entire line to stderr or stdout
-        system("echo \"Missing data in line: " $0 "\" >&2");
-        next;  # Skip printing this line to the output file
+  #  error_txt="";
+
+   # if ($1 == "" || $2 == "" || $3 == "" || $4 == "" || $5 == "") {
+    #    # Use system to print the entire line to stderr or stdout
+   #     error_txt=error_txt "Empty data in line: ";
+  #  }
+
+    #Normalized invalid fields to N/A
+    if ($5 ~ /^[0-9]+$/) {
+	    $5 = "N/A"
     }
+
+    if ($6 == "" || $7 == "") {
+   #     error_txt = error_txt " Killing or injured field is empty: ";
+	next;
+    }
+
     print $0;  # Print the line to the output file if no fields are missing
 }' $cleaned_file > $no_missing
 
